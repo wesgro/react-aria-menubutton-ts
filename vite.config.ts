c35bc37817e0ec2b2893ce953c5a9e461d4392b3
Jacob <snowcave@gmail.com>
2024-07-14 03:00:49 +0000

    fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-aria-menubutton-ts/',
   test: {
     environment: 'happy-dom',
     setupFiles: ['./vitest-setup.ts'],
